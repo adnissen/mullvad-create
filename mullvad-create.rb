dcfcddff674586e380ab19b1934dd22120a02ce6
adnissen <andrew_nissen@yahoo.com>
2014-01-31 18:24:01 -0500

    now just returns the account number

--- mullvad-create.rb
+++ mullvad-create.rb
@@ -1,7 +1,9 @@
 require 'unirest'
+require 'nokogiri'
 
 response = Unirest.post "https://mullvad.net/en/account/", 
                         headers:{ "Accept" => "application/text" }, 
                         parameters:{ :create_account => "create" }
 
-puts response.raw_body # Unparsed body
+doc = Nokogiri::HTML.parse(response.body) # Unparsed body
+puts "Your new trial account number: " + doc.search('input').first['value']
